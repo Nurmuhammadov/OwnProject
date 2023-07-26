2a988a14e45a8eb7641e9c0fab40cd9a02c4cf8f
Nurmuhammad <nurmuhammadov777@gmail.com>
2023-07-26 09:18:22 +0500

    api

--- api/models.py
+++ api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Question(models.Model):
+    question = models.CharField(max_length=20)
+    answer_count = models.IntegerField(default=3)
+    answer_given = models.IntegerField(default=0)
+
+
+class Answer(models.Model):
+    answer = models.CharField(max_length=50)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+
+class User(User):
+    age = models.IntegerField()
